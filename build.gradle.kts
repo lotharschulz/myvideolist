5b52d71fa557ed254257b6e3a50313c00e0b38fd
Lothar Schulz <lothar.sch@gmx.de>
2020-10-04 09:28:12 +0200

    chore: chapter 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
 dependencies {
     implementation(kotlin("stdlib-js"))
 
-/*    //React, React DOM + Wrappers (chapter 3)
+    //React, React DOM + Wrappers (chapter 3)
     implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.0")
     implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.0")
     implementation(npm("react", "16.13.1"))
@@ -33,7 +33,7 @@ dependencies {
     implementation(npm("styled-components", "~5.1.1"))
     implementation(npm("inline-style-prefixer", "~6.0.0"))
 
-    //Video Player (chapter 7)
+/*    //Video Player (chapter 7)
     implementation(npm("react-player", "~2.6.0"))
 
     //Share Buttons (chapter 7)
