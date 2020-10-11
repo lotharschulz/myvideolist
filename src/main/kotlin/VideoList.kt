0ab70e179c85e649aecc0d6ac4b80200cc1d2263
Lothar Schulz <lothar.sch@gmx.de>
2020-10-11 14:59:07 +0200

    chore: initial video set

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,4 +1,6 @@
+import kotlinx.browser.window
 import kotlinx.html.js.onClickFunction
+import kotlinx.html.js.onLoadFunction
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -12,12 +14,32 @@ external interface VideoListProps: RProps {
     var onSelectVideo: (Video) -> Unit
 }
 
-class VideoList: RComponent<VideoListProps, RState>() {
+external interface VideoListState: RState {
+    var initialState:Boolean
+}
+
+class VideoList: RComponent<VideoListProps, VideoListState>() {
+
+    override fun VideoListState.init() {
+        initialState = true
+    }
+
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
                 attrs {
+                    if (state.initialState) {
+                        props.onSelectVideo(
+                            Video(
+                                1,
+                                "Open Source CI/CD components for GitHub Actions",
+                                "Lothar Schulz",
+                                "https://youtu.be/26E9hRKkqw4"
+                            )
+                        )
+                        state.initialState = false
+                    }
                     onClickFunction = {
                         props.onSelectVideo(video)
                     }
